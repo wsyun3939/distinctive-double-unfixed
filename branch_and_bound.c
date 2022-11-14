ba4e1c00497f681e35106d149e87785c6ae91441
wtoshi3939 <91554668+wsyun3939@users.noreply.github.com>
2022-11-14 15:00:45 +0900

    main

--- branch_and_bound.c
+++ branch_and_bound.c
@@ -14,6 +14,16 @@ int branch_and_bound(IntDequeue *q, int UB, int UB_cur, int LB, direction Dir, c
 	static int SecondPosition = 0;
 	static int MinRelocation = 0;
 	static int BlockSpace = 0;
+
+	if (depth + LB == UB_cur - 1)
+	{
+		int UB_temp = UpperBound(q);
+		if (UB > UB_temp + depth && UB_temp != -1)
+		{
+			UB = UB_temp + depth;
+		}
+	}
+
 	if (UB == UB_cur)
 	{
 		depth = 0;
